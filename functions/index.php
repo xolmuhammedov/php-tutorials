55efff1c3f2ddabd346faf5c2788c51e3e520b0b
MuhammadYusuf <136478219+xolmuhammedov@users.noreply.github.com>
2024-05-30 11:59:54 +0000

    trying to understand strcit mode

--- functions/index.php
+++ functions/index.php
@@ -1,5 +1,7 @@
 <?php
-function getStatus($paymentStatus , $showMessage = true) : string {
+decalre(strict_types=1);
+function getStatus(int $paymentStatus , bool $showMessage = true) : 
+  ?string {
   $message = match($paymentStatus) {
     1 => 'Success',
     2 => 'Denied',
@@ -8,10 +10,12 @@ function getStatus($paymentStatus , $showMessage = true) : string {
 
   if($showMessage){
     var_dump($message);
+    return null;
   }
 
   return $message;
 }
 $statusMessage = getStatus(1);
 var_dump($statusMessage);
+
 ?>
