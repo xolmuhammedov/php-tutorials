7af5dd96af903145072dc07d5899d28187fd0bc8
MuhammadYusuf <136478219+xolmuhammedov@users.noreply.github.com>
2024-06-02 12:54:14 +0000

    for and while and do while loops

--- for and while loops/home.php
+++ for and while loops/home.php
@@ -1,20 +1,36 @@
 <?php
+// echo "Hello World<br>";
+// $money = 100;
+// $money += 10;
+// $a = 1;
+// if($money == 110){
+//   echo "You have enought money<br>";
+// }else{
+//   echo "You don't have enough money";
+// }
+
+// for($i = 0; $i < 10; $i++){
+//   echo $i;
+// }
+// echo "<br>";
+// while($a < 10){
+//   echo $a;
+//   $a++;
+// }
+
+
+
 echo "Hello World<br>";
-$money = 100;
-$money += 10;
-$a = 1;
-if($money == 110){
-  echo "You have enought money<br>";
+if(true === true){
+  echo "that's true";
 }else{
-  echo "You don't have enough money";
+  echo "that's not true";
 }
 
-for($i = 0; $i < 10; $i++){
-  echo $i;
+for($i = 0; $i < 100; $i++){
+  echo "<br>" , $i;
 }
-echo "<br>";
-while($a < 10){
-  echo $a;
-  $a++;
-}
+
+$arr = ["Hello"];
+
 ?>
