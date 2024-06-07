fc7106b71fc6d0da866717a351542cf54582091e
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-06-07 15:58:42 +0500

    hi

--- functions/index.php
+++ functions/index.php
@@ -1,21 +1,4 @@
 <?php
-decalre(strict_types=1);
-function getStatus(int $paymentStatus , bool $showMessage = true) : 
-  ?string {
-  $message = match($paymentStatus) {
-    1 => 'Success',
-    2 => 'Denied',
-    default => 'Unknown'
-  };
-
-  if($showMessage){
-    var_dump($message);
-    return null;
-  }
-
-  return $message;
-}
-$statusMessage = getStatus(1);
-var_dump($statusMessage);
+echo "it is working";
 
 ?>
